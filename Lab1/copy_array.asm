4373b52e30ca32f518f798f55191d9312534ad25
rushil <rushiljalal@gmail.com>
2024-07-26 12:15:54 +0530

    add comments

--- Lab1/copy_array.asm
+++ Lab1/copy_array.asm
@@ -9,17 +9,17 @@ __Vectors
 	ENTRY
 	EXPORT Reset_Handler
 Reset_Handler
-	LDR R0, =N
-	LDR R2, =M
-	MOV R3, #5
+	LDR R0, =N;/address of N
+	LDR R2, =M;/address of M
+	MOV R3, #5;/loop counter
  
-BACK LDR R1, [R0], #4
-	STR R1, [R2], #4
-	SUBS R3, #1
+BACK LDR R1, [R0], #4;/load value of R0 to R1(0x1), shift R0 by 4 bits
+	STR R1, [R2], #4;/store value of R1 in R2(containing address of M) and shift this address by 4
+	SUBS R3, #1;/SUBS changes the Z flag
 	BNE BACK
  
 STOP B STOP
-N DCD 0x1, 0x2, 0x3, 0x4, 0x5
-	AREA mydata, DATA, READWRITE
+N DCD 0x1, 0x2, 0x3, 0x4, 0x5;/Define Constant Data(32bit)
+	AREA mydata, DATA, READWRITE;/anything defined in the READWRITE area is initialised to 0 at exec time
 M DCD 0
 	END
