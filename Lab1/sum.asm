693ad6fe8585a5447c7a2b2f305936ed8662467f
rushil <rushiljalal@gmail.com>
2024-08-26 12:41:22 +0530

    fixed lab 1

--- Lab1/sum.asm
+++ Lab1/sum.asm
@@ -1,33 +1,33 @@
-	AREA RESET, DATA, READONLY
-	EXPORT __VECTORS
-__VECTORS
-	DCD 0X00010001
-	DCD Reset_Handler
-	ALIGN
-	AREA mycoed, CODE,READONLY
-	ENTRY
-	EXPORT Reset_Handler
+    AREA RESET, DATA, READONLY
+    EXPORT __Vectors
+
+__Vectors
+    DCD 0x10001000
+    DCD Reset_Handler
+    ALIGN
+    AREA mycode, CODE, READONLY
+    ENTRY
+    EXPORT Reset_Handler
+	
 Reset_Handler
-	LDR R0,=NUM
-	LDR R5,=M
-UP LDR R6,[R5],#4
-	LDR R1,[R0],#4
-	LSR R2,R1,#1;R2=R1/2
-UP2 MOV R3,R2;MOVES R2 INTO R3
-UP3 CMP R1,R3
-	SUBLT R3,R1
-	SUBGT R1,R3
-	BNE UP3
-	CMP R1,R2
-	BEQ UP
-	SUBS R2,#1
-	BNE UP2
-	STREQ R1,[R6],#4
-	BEQ UP
-	
+    ; subtract 2 64-bit numbers and store in memory
+    LDR R0, =N1
+    LDR R1, =N2
+    LDR R2, =M
+	LDR R5, =2
+    
+LOOP LDR R6, [R0], #4
+    LDR R7, [R1], #4
+    SUBS R3, R6, R7
+	STR R3, [R2], #4
+	SUBS R5, #1
+	BNE LOOP
+    
+    
 STOP B STOP
-	
-NUM DCD #1,#2,#3,#4,#5,#6,#7,#8,#9,#10
-M DCD 0X10000000
-	AREA mydata, DATA, READWRITE
-	END
+
+N1 DCD 0xFFFFFFFF, 0x00000002  ; 64-bit number: 0x00000002FFFFFFFF
+N2 DCD 0x00000003, 0x00000004  ; 64-bit number: 0x0000000400000003
+    AREA mydata, DATA, READWRITE
+M DCD 0, 0  ; Space for 64-bit result
+    END
